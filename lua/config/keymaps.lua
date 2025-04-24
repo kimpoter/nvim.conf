d90d39394ccccbebc4ddf602565e6685372d198e
sozy <thinpotatogithub@gmail.com>
2025-04-24 13:13:00 +0700

    feat: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,19 @@ vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent dir. in f
 vim.keymap.set("n", "<leader>cf", function()
 	require("conform").format({ lsp_format = "fallback" })
 end, { desc = "Format current file " })
+
+--Split window
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Horizontal split" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Vertical split" })
+
+-- Move between splits
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+
+-- Resize splits
+vim.keymap.set("n", "<M-Up>", ":resize +2<CR>", { desc = "Increase height" })
+vim.keymap.set("n", "<M-Down>", ":resize -2<CR>", { desc = "Decrease height" })
+vim.keymap.set("n", "<M-Left>", ":vertical resize -2<CR>", { desc = "Decrease width" })
+vim.keymap.set("n", "<M-Right>", ":vertical resize +2<CR>", { desc = "Increase width" })
